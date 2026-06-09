ba67d8bbfb8fe0f2939dd10858016aa3ed1c7ecd
jannatul040 <fardous083@gmail.com>
2026-06-09 11:06:43 +0600

    Update YouDo8.java

--- Task 2/YouDo8.java
+++ Task 2/YouDo8.java
@@ -1,1 +1,33 @@
+class Item{
+    String name;
+    double price; 
+    Item(String name, double price){
+        this.name = name;
+        this.price = price;
+    }
+    
+}
+class Order{
+    Item item1;
+    Item item2;
 
+    Order(Item item1,Item item2){
+        this.item1=item1;
+        this.item2= item2;
+    }
+    void printBill(){
+        double total = item1.price + item2.price;
+        System.out.println("Total Bill: "+total);
+
+    }
+}
+
+
+public class Main8 {
+   public static void main(String[] args) {
+    Item mouse = new Item("Mouse", 20.0);
+    Item keyboard = new Item("keyboard",45.0);
+    Order order = new Order(mouse,keyboard);
+    order.printBill();
+   }
+}
