4ef182df0a186a1e1bc7b5dcd18217fe25209606
jannatul040 <fardous083@gmail.com>
2026-06-23 10:39:09 +0600

    Add TeamMember hierarchy with Employee and Contractor

--- Task 4/Main2.java
+++ Task 4/Main2.java
@@ -1,1 +1,39 @@
 
+abstract class TeamMember {
+    protected String name;
+    public TeamMember(String name){
+        this.name= name;
+    }
+    
+
+}
+interface BonusEligible{
+    void calculateBonus();
+
+}
+class Employee extends TeamMember implements BonusEligible{
+    public Employee(String name){
+    super(name);
+}
+@Override
+public void calculateBonus(){
+    System.out.println("calculating standard employee bonus...");
+}
+}
+class Contractor extends TeamMember {
+    public Contractor(String name){
+        super(name);
+    }
+    public void work(){
+        System.out.println("Contractor is working on a project.");
+    }
+}
+public class Main2{
+    public static void main(String[] args) {
+        Employee emp = new Employee("John");
+        Contractor con = new Contractor("Alice");
+        emp.calculateBonus();
+        con.work();
+    }
+}
+
