1046d9dfec86437b236559a6a5258c3ea42e2d26
jannatul040 <fardous083@gmail.com>
2026-06-30 08:54:03 +0600

    Update Main3.java

--- Task 4/Main3.java
+++ Task 4/Main3.java
@@ -1,1 +1,60 @@
+// Interface for printing
+interface Printer {
+    void printDocument();
+}
 
+// Interface for scanning
+interface Scanner {
+    void scanDocument();
+}
+
+// Interface for faxing
+interface FaxMachine {
+    void faxDocument();
+}
+
+// Multi-function copier supports all features
+class MultiFunctionCopier implements Printer, Scanner, FaxMachine {
+
+    @Override
+    public void printDocument() {
+        System.out.println("Printing...");
+    }
+
+    @Override
+    public void scanDocument() {
+        System.out.println("Scanning...");
+    }
+
+    @Override
+    public void faxDocument() {
+        System.out.println("Faxing...");
+    }
+}
+
+// Basic printer only supports printing
+class BasicPrinter implements Printer {
+
+    @Override
+    public void printDocument() {
+        System.out.println("Printing...");
+    }
+}
+
+// Main class
+public class Main {
+    public static void main(String[] args) {
+
+        // Basic Printer
+        Printer basicPrinter = new BasicPrinter();
+        basicPrinter.printDocument();
+
+        System.out.println();
+
+        // Multi-Function Copier
+        MultiFunctionCopier copier = new MultiFunctionCopier();
+        copier.printDocument();
+        copier.scanDocument();
+        copier.faxDocument();
+    }
+}
