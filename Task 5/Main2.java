b0d7b868bffa85dd3fe47c8483589dbebfb97cf1
jannatul040 <fardous083@gmail.com>
2026-06-30 11:35:06 +0600

    Update Main2.java

--- Task 5/Main2.java
+++ Task 5/Main2.java
@@ -1,1 +1,36 @@
+interface MessageSender{
+    void sendMessage(String message) ;   
+}
+class EmailSender implements MessageSender{
+    public void sendMessage(String message){
+        System.out.println("Sending email: "+message);
+    }
+}
+class SmsSender implements MessageSender{
+    public void sendMessage(String message){
+        System.out.println("Sending SMS: "+message);
+    }
+}
 
+class NotificationService{
+    private MessageSender sender;
+
+    public NotificationService(MessageSender sender){
+        this.sender = sender;
+
+    }
+    public void alertUser(String msg){
+        sender.sendMessage(msg);
+    }
+}
+public class Main2 {
+public static void main(String[] args) {
+    MessageSender emailSender = new EmailSender();
+    NotificationService service1 = new NotificationService(emailSender);
+    service1.alertUser("welcome via Email");
+    MessageSender smsSender = new SmsSender();
+    NotificationService service2 = new NotificationService(smsSender);
+    service2.alertUser("Welcome via SMS");
+}
+    
+}
